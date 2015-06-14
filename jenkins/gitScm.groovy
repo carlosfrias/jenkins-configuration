dfb137b0c460828adfec55e1a0677cc699a4ba0d
Carlos Frias <carlos.frias.01@gmail.com>
2015-06-14 08:02:47 -0400

    working with git configuration

--- jenkins/gitScm.groovy
+++ jenkins/gitScm.groovy
@@ -4,60 +4,18 @@
 configPrefixMessage = "CUSTOM CONFIGURATION"
 
 println "$configPrefixMessage: Configuring Git"
-import jenkins.model.Jenkins
-import hudson.plugins.sonar.*
-import hudson.plugins.sonar.model.TriggersConfig;
-import hudson.tools.*
+import hudson.plugins.git.*
 
-println "$configPrefixMessage: Loading Git properties..."
-prop = [:]
-prop.name = System.env.SONARQUBE_NAME
-prop.disabled = false
-prop.serverUrl = "http://${System.env.SONARQUBE_PORT_9000_TCP_ADDR}:${System.env.SONARQUBE_PORT_9000_TCP_PORT}"
-prop.databaseUrl = System.env.SONARQUBE_ENV_JDBC_URL
-prop.databaseLogin = System.env.SONARQUBE_ENV_JDBC_USERNAME
-prop.databasePassword = System.env.SONARQUBE_ENV_JDBC_PASSWORD
-prop.sonarRunnerVersion = System.env.SONAR_RUNNER_VERSION
-//prop.mojoVersion = null
-//prop.additionalProperties = null
-//prop.triggers = null
-//prop.sonarLogin = null
-//prop.sonarPassword = null
+descriptor = Jenkins.instance.getDescriptorByType(hudson.plugins.git.GitSCM.DescriptorImpl.class)
+println "$configPrefixMessage: Configuring GitSCM..."
 
-println "$configPrefixMessage: Creating SonarInstallation instance..."
-updatedSonarInstall = new SonarInstallation(prop?.name,
-        prop?.disabled,
-        prop?.serverUrl,
-        prop?.databaseUrl,
-        prop?.databaseLogin,
-        prop?.databasePassword,
-        prop?.mojoVersion,
-        prop?.additionalProperties,
-        prop?.triggers as TriggersConfig,
-        prop?.sonarLogin,
-        prop?.sonarPassword)
+println "$configPrefixMessage: Setting global git properties..."
+descriptor.globalConfigName = "Carlos Frias"
+descriptor.globalConfigEmail = "carlos.frias.01@gmail.com"
+descriptor.createAccountBasedOnEmail = false
 
-println "$configPrefixMessage: Replacing SonarInstallation with the updated instance..."
-descriptor = Jenkins.instance.getDescriptorByType(SonarPublisher.DescriptorImpl.class)
-descriptor.installations = updatedSonarInstall
 
-println "$configPrefixMessage: Saving updated SonarInstallation instance... "
+println "$configPrefixMessage: Saving Git Configuration."
 descriptor.save()
 
-println "$configPrefixMessage: Creating SonarRunerInstaller instance..."
-installer = new SonarRunnerInstaller(prop?.sonarRunnerVersion)
-
-println "$configPrefixMessage: Creating InstallSourceProperty instance..."
-property = new InstallSourceProperty([installer])
-
-println "$configPrefixMessage: Creating SonarRunnerInstallation instance..."
-runner = new SonarRunnerInstallation(prop?.name, null, [property])
-
-println "$configPrefixMessage: Obtaining the SonarRunnerInstallation Descriptor instance..."
-descriptor = Jenkins.instance.getDescriptorByType(SonarRunnerInstallation.DescriptorImpl.class)
-
-println "$configPrefixMessage: Saving SonarRnnerInstallation..."
-descriptor.installations = runner
-descriptor.save()
-
-println "$configPrefixMessage: Sonar configuration complete."
+println "$configPrefixMessage: Git Configuration Complete."
--- jenkins/init.groovy
+++ jenkins/init.groovy
@@ -1,7 +1,19 @@
 /**
  * Created by carlosfrias on 6/13/15.
  */
+
 sonar = new File("${System.env.JENKINS_HOME}/sonar.groovy")
-if (sonar.exists())
+if (sonar.exists()) {
+    println "INIT: Loading Sonar Configuration..."
     evaluate(sonar)
+    println "INIT: Sonar Configuration Loaded."
+}
+
+gitScm = new File("${System.env.JENKINS_HOME}/gitScm.groovy")
+if(gitScm.exists()) {
+    println "INIT: Loading GitSCM Configuration..."
+    evaluate(gitScm)
+    prinlnt "INIT: GitSCM Configuration Loaded."
+}
+
 
